398fdb463eefc506ff95bdc4bea3a81b2aba2b2a
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-09-02 21:13:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,61 +7,52 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 };
 
-
 /*=====scroll section active link======*/
-let sections =  document.querySelectorAll('section');
+let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+  let top = window.scrollY;
+
   sections.forEach(sec => {
-    let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
-    if(top >= offset && top < offset + height) {
-      navlinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(link => {
+        link.classList.remove('active');
+        document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
       });
-
-    };
+    }
   });
 
+  /*=====sticky navbar======*/
+  let header = document.querySelector('.header');
+  header.classList.toggle('sticky', window.scrollY > 100);
 
-/*=====sticky navbar======*/
-let header = document.querySelector('.header');
-
-header.classList.toggle('sticky', window.scrollY > 100);
-
-
-/*=====remove menu icon navbar when click scroll======*/
-
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
-
-
-
+  /*=====remove menu icon navbar when click scroll======*/
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
 
 /*=====swiper======*/
-
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-/*=====drak light mode======*/
+/*=====dark light mode======*/
 let darkModeIcon = document.querySelector('#darkMode-icon');
 
 darkModeIcon.onclick = () => {
@@ -71,7 +62,6 @@ darkModeIcon.onclick = () => {
 
 /*=====scroll reveal======*/
 ScrollReveal({
-  //reset:true,
   distance: '80px',
   duration: 2000,
   delay: 200,
